1a0b3e968c70a4e4dee98e3cbbd8217e107b0dce
John Diego <jadiego@uw.edu>
2017-04-04 23:55:53 -0700

    starter code for story handler

---
+++ apiserver/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "80"
+
+const (
+	apiRoot     = "/v1/"
+	apiTimeline = apiRoot + "timeline"
+)
+
+//main is the main entry point for this program
+func main() {
+	//read and use the following environment variables
+	//when initializing and starting your web server
+	// PORT - port number to listen on for HTTP requests (if not set, use defaultPort)
+	// HOST - host address to respond to (if not set, leave empty, which means any host)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		fmt.Println("Port number not set. Defaulting to port: " + defaultPort)
+		port = defaultPort
+	}
+	host := os.Getenv("HOST")
+	if len(host) == 0 {
+		fmt.Println("Host not set. Defaulting to empty host")
+		host = ""
+	}
+
+	//add your handlers.SummaryHandler function as a handler
+	//for the apiSummary route
+	//HINT: https://golang.org/pkg/net/http/#HandleFunc
+	http.HandleFunc(apiSummary, handlers.EditStoryHandler)
+
+	//start your web server and use log.Fatal() to log
+	//any errors that occur if the server can't start
+	//HINT: https://golang.org/pkg/net/http/#ListenAndServe
+	fmt.Printf("server is listening at %s:%s...\n", host, port)
+	log.Fatal(http.ListenAndServe(host+":"+port, nil))
+
+}
