5c1e7bf68a28664bff13a5e45f411d1fc52cf4df
John Diego <jadiego@uw.edu>
2017-04-06 00:14:39 -0700

    add home screen

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,16 +1,93 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import { Menu } from 'semantic-ui-react'
+import { Menu, Container, Header, Image, Button, Grid } from 'semantic-ui-react'
+
+import image from './img/image.png';
+import mediaparagraph from './img/mediaparagraph.png';
+import paragraph from './img/paragraph.png';
 
 class TopNavBar extends Component {
+	state = {}
+
+	handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+	render() {
+		const { activeItem } = this.state
+
+		return (
+			<Menu fixed='top' inverted>
+				<Container>
+					<a href="#">
+						<Menu.Item link header>
+							Bloom
+						</Menu.Item>
+					</a>
+					<Menu.Menu position='right'>
+						<Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} link>
+							Home
+       					</Menu.Item>
+						<Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick} link>
+							About
+        				</Menu.Item>
+						<Menu.Item name='account' active={activeItem === 'account'} onClick={this.handleItemClick} link>
+							Account
+        				</Menu.Item>
+					</Menu.Menu>
+				</Container>
+			</Menu>
+		)
+	}
+}
+
+class BottomFooter extends Component {
+
 	render() {
 		return (
-			<Menu>
-				<Menu.Header>
-				</Menu.Header>
-				<Menu className="Item"></Menu>
+			<Menu inverted content='Footer' id="footer">
+
 			</Menu>
+		)
+	}
+}
+
+class StoriesGrid extends Component {
+	render() {
+		return (
+			<Container>
+				<Grid celled>
+					<Grid.Row>
+						<Grid.Column width={3}>
+							<Image src={image} />
+						</Grid.Column>
+						<Grid.Column width={13}>
+							<Image src={paragraph} />
+						</Grid.Column>
+					</Grid.Row>
+					<Grid.Row>
+						<Grid.Column width={3}>
+							<Image src={paragraph} />
+						</Grid.Column>
+						<Grid.Column width={10}>
+							<Image src={mediaparagraph} />
+						</Grid.Column>
+						<Grid.Column width={3}>
+							<Image src={mediaparagraph} />
+						</Grid.Column>
+					</Grid.Row>
+					<Grid.Row>
+						<Grid.Column width={3}>
+							<Image src={image} />
+						</Grid.Column>
+						<Grid.Column width={10}>
+							<Image src={paragraph} />
+						</Grid.Column>
+						<Grid.Column width={3}>
+							<Image src={image} />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</Container>
 		)
 	}
 }
@@ -18,8 +95,18 @@ class TopNavBar extends Component {
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<TopNavBar></TopNavBar>
+			<div>
+				<TopNavBar />
+				<Container fluid className='body'>
+					<Container id="jumbo" textAlign='center'>
+						<Image centered={true} src='https://www.pissedconsumer.com/themes/foundation/images/featured_placeholder-400x300.png' size='small' />
+						<Header size='huge'>You have the power to own your story</Header>
+						<Button size='small'>Submit Your Own</Button>
+					</Container>
+					<StoriesGrid />
+					<StoriesGrid />
+				</Container>
+				<BottomFooter></BottomFooter>
 			</div>
 		);
 	}
