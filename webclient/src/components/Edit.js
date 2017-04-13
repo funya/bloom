8162299ecfa10b54458470aad7dba15a6c2c5b89
John Diego <jadiego@uw.edu>
2017-04-12 20:12:26 -0700

    take out right side bar

--- webclient/src/components/Edit.js
+++ webclient/src/components/Edit.js
@@ -1,63 +1,48 @@
 import React, { Component } from 'react';
-import { Container, Grid, Menu, Sidebar, Segment, Button, Icon, Form, Modal } from 'semantic-ui-react'
+import { Container, Grid, Menu, Sidebar, Segment, Button, Icon } from 'semantic-ui-react'
 
 import VisibleTimeline from '../containers/VisibleTimeline'
 
 
 class Edit extends Component {
-    state = { leftSidebarVisible: true, rightSidebarVisible: false }
-
+    state = { visible: true }
     componentDidMount() {
         document.title = "Bloom | Edit";
     }
 
-    toggleLeftSidebar = () => this.setState({ ...this.state, leftSidebarVisible: !this.state.leftSidebarVisible })
-
-    toggleRightSidebar = () => {
-        this.setState({ ...this.state, rightSidebarVisible: !this.state.rightSidebarVisible })
-
-    }
-
+    toggleSidebar = () => this.setState({ visible: !this.state.visible })
 
     render() {
-        const { leftSidebarVisible, rightSidebarVisible } = this.state
+        const { visible } = this.state
         return (
             <Container fluid={true}>
-                <Sidebar.Pushable as={Segment}>
-                    <Sidebar as={Segment} animation='overlay' visible={rightSidebarVisible} direction='right' width='very wide' vertical >
-                        sdf
+                <Sidebar.Pushable as={Segment} id='main-edit-container'>
+                    <Sidebar as={Menu} animation='overlay' visible={visible} width='wide' vertical>
+                        <Menu.Item name='add-section'>
+                            <Icon name='add '/>
+                            Add Section
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Icon name='trash outline'/>
+                            Delete Section
+                        </Menu.Item>
+                        <Menu.Item >
+                            <Icon name='edit'/>
+                            Edit Section
+                        </Menu.Item>
                     </Sidebar>
                     <Sidebar.Pusher>
-                        <Sidebar.Pushable as={Segment} id='main-edit-container'>
-                            <Sidebar as={Menu} animation='push' visible={leftSidebarVisible} width='wide' vertical>
-                                <Menu.Item name='add-section' onClick={this.toggleRightSidebar}>
-                                    <Icon name='add' />
-                                    Add Section
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <Icon name='trash outline' />
-                                    Delete Section
-                                </Menu.Item>
-                                <Menu.Item >
-                                    <Icon name='edit' />
-                                    Edit Section
-                                </Menu.Item>
-                            </Sidebar>
-                            <Sidebar.Pusher>
-                                <Segment as={Container} padded fluid>
-                                    <Container textAlign='right'>
-                                        <Button basic compact onClick={this.toggleLeftSidebar}>Toggle Sidebar</Button>
-                                        <Button basic compact>Toggle Timeline View</Button>
-                                    </Container>
-                                    <Container fluid>
-                                        <VisibleTimeline />
-                                    </Container>
-                                </Segment>
-                            </Sidebar.Pusher>
-                        </Sidebar.Pushable>
+                        <Segment as={Container} padded fluid>
+                            <Container textAlign='right'>
+                                <Button basic compact onClick={this.toggleSidebar}>Toggle Sidebar</Button>
+                                <Button basic compact>Toggle Timeline View</Button>
+                            </Container>
+                            <Container fluid>
+                                <VisibleTimeline />
+                            </Container>
+                        </Segment>
                     </Sidebar.Pusher>
                 </Sidebar.Pushable>
-
             </Container>
         )
     }
