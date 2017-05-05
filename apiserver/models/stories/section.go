33f6d5b434926718c710288a51d95883119b282b
John Diego <jadiego@uw.edu>
2017-05-04 17:27:26 -0700

    initial section struct

---
+++ apiserver/models/stories/section.go
@@ -0,0 +1,47 @@
+package stories
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jadiego/bloom/apiserver/models/users"
+)
+
+//SectionID defines the type of section IDs
+type SectionID string
+
+//Section represents a single section within a story
+type Section struct {
+	ID        SectionID    `json:"id" bson:"_id"`
+	StoryID   StoryID      `json:"storyid" bson:"story_id"`
+	CreatorID users.UserID `json:"creatorid" bson:"user_id"`
+	Body      string       `json:"body"`
+	CreatedAt time.Time    `json:"createdAt"`
+	EditedAt  time.Time    `json:"editedAt"`
+}
+
+//NewSection represents the fields a new section the client wants
+//to add to a story
+type NewSection struct {
+	StoryID StoryID `json:"storyid" bson:"story_id"`
+	Body    string  `json:"body"`
+}
+
+//SectionUpdates represents updates one can make to a section
+type SectionUpdates struct {
+	Body string `json:"body"`
+}
+
+//Validate validates the new section
+func (ns *NewSection) Validate() error {
+	//ensure the name is not empty
+	if len(ns.StoryID) < 1 {
+		return fmt.Errorf("error storyID cant be empty, no context on which story to add this section to")
+	}
+
+	if len(ns.Body) < 1 {
+		return fmt.Errorf("error invalid body, body can't be empty")
+	}
+
+	return nil
+}
