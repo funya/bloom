4ff379fbd8b86a0737f649e14f3ff380b551d8cb
John Diego <jadiego@uw.edu>
2017-05-04 20:49:11 -0700

    seperate stories and sections in mongostore struct

--- apiserver/models/stories/mongostore.go
+++ apiserver/models/stories/mongostore.go
@@ -8,15 +8,16 @@ import (
 
 //MongoStore represents a stories.Store backed by MongoDB
 type MongoStore struct {
-	Session        *mgo.Session
-	DatabaseName   string
-	CollectionName string
+	Session                *mgo.Session
+	DatabaseName           string
+	StoriesCollectionName  string
+	SectionsCollectionName string
 }
 
 //GetAllStories returns all public stories
 func (ms *MongoStore) GetAllStories() ([]*Story, error) {
 	stories := []*Story{}
-	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).Find(nil).All(&stories)
+	err := ms.Session.DB(ms.DatabaseName).C(ms.StoriesCollectionName).Find(nil).All(&stories)
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +27,7 @@ func (ms *MongoStore) GetAllStories() ([]*Story, error) {
 //GetStoryByID returns the Story with the given ID
 func (ms *MongoStore) GetStoryByID(id StoryID) (*Story, error) {
 	story := &Story{}
-	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).FindId(id).One(story)
+	err := ms.Session.DB(ms.DatabaseName).C(ms.StoriesCollectionName).FindId(id).One(story)
 	if err == mgo.ErrNotFound {
 		return nil, ErrStoryNotFound
 	}
@@ -39,7 +40,7 @@ func (ms *MongoStore) InsertStory(id users.UserID, newStory *NewStory) (*Story, 
 	s := newStory.ToStory()
 	s.CreatorID = id
 	s.ID = StoryID(bson.NewObjectId().Hex())
-	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).Insert(s)
+	err := ms.Session.DB(ms.DatabaseName).C(ms.StoriesCollectionName).Insert(s)
 	return s, err
 }
 
@@ -47,13 +48,13 @@ func (ms *MongoStore) InsertStory(id users.UserID, newStory *NewStory) (*Story, 
 func (ms *MongoStore) GetAllStorySections(id StoryID) ([]*Section, error) {
 	sections := []*Section{}
 	query := bson.M{"story_id": id}
-	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).Find(query).All(&sections)
+	err := ms.Session.DB(ms.DatabaseName).C(ms.SectionsCollectionName).Find(query).All(&sections)
 	return sections, err
 }
 
 //UpdateStory applies StoryUpdates to the current Story
 func (ms *MongoStore) UpdateStory(updates *StoryUpdates, currentStory *Story) error {
-	col := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName)
+	col := ms.Session.DB(ms.DatabaseName).C(ms.StoriesCollectionName)
 	storyupdates := bson.M{"$set": updates}
 	err := col.UpdateId(currentStory.ID, storyupdates)
 	return err
@@ -61,12 +62,12 @@ func (ms *MongoStore) UpdateStory(updates *StoryUpdates, currentStory *Story) er
 
 //DeleteStory deletes the story and sections within it
 func (ms *MongoStore) DeleteStory(id StoryID) error {
-	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).RemoveId(id)
+	err := ms.Session.DB(ms.DatabaseName).C(ms.StoriesCollectionName).RemoveId(id)
 	if err != nil {
 		return err
 	}
 
-	_, err = ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).RemoveAll(bson.M{"story_id": id})
+	_, err = ms.Session.DB(ms.DatabaseName).C(ms.SectionsCollectionName).RemoveAll(bson.M{"story_id": id})
 	if err != nil {
 		return err
 	}
@@ -79,13 +80,13 @@ func (ms *MongoStore) InsertSection(newSection *NewSection, id users.UserID) (*S
 	s := newSection.ToSection()
 	s.CreatorID = id
 	s.ID = SectionID(bson.NewObjectId().Hex())
-	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).Insert(s)
+	err := ms.Session.DB(ms.DatabaseName).C(ms.SectionsCollectionName).Insert(s)
 	return s, err
 }
 
 //UpdateSection applies the SectionUpdates to the current section
 func (ms *MongoStore) UpdateSection(updates *SectionUpdates, currentSection *Section) error {
-	col := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName)
+	col := ms.Session.DB(ms.DatabaseName).C(ms.SectionsCollectionName)
 	sectionupdates := bson.M{"$set": updates}
 	err := col.UpdateId(currentSection.ID, sectionupdates)
 	return err
@@ -93,6 +94,37 @@ func (ms *MongoStore) UpdateSection(updates *SectionUpdates, currentSection *Sec
 
 //DeleteSection deletes the section
 func (ms *MongoStore) DeleteSection(id SectionID) error {
-	err := ms.Session.DB(ms.DatabaseName).C(ms.CollectionName).RemoveId(id)
+	err := ms.Session.DB(ms.DatabaseName).C(ms.SectionsCollectionName).RemoveId(id)
 	return err
+}
+
+//NewMongoStore constructs a new MongoStore, using the provided
+//addr. If the `mongoAddr` is nil, it will use a default port and host
+func NewMongoStore(mongoAddr, DBName, StoriesCollectName, SectionsCollectName string) (*MongoStore, error) {
+	if len(mongoAddr) == 0 {
+		mongoAddr = "localhost:27017"
+	}
+
+	sess, err := mgo.Dial(mongoAddr)
+	if err != nil {
+		return nil, err
+	}
+	if len(DBName) == 0 {
+		DBName = "bloom"
+	}
+
+	if len(StoriesCollectName) == 0 {
+		StoriesCollectName = "stories"
+	}
+
+	if len(SectionsCollectName) == 0 {
+		SectionsCollectName = "sections"
+	}
+
+	return &MongoStore{
+		Session:                sess,
+		DatabaseName:           DBName,
+		StoriesCollectionName:  StoriesCollectName,
+		SectionsCollectionName: SectionsCollectName,
+	}, nil
 }
