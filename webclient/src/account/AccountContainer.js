ac48abfcd7f4037e17439dab87f870283041a157
John Diego <jadiego@uw.edu>
2017-05-10 01:25:31 -0700

    add handler to delete story

--- webclient/src/account/AccountContainer.js
+++ webclient/src/account/AccountContainer.js
@@ -12,22 +12,20 @@ class AccountContainer extends Component {
             stories: [],
             loading: false,
             openModal: false,
-            loadingform: false
+            loadingform: false,
+            deleteModalOpen: false,
         }
     }
 
-    showmodal = (dimmer) => () => this.setState({ dimmer, openModal: true })
-    closemodal = () => this.setState({ openModal: false })
-
-    componentDidMount = () => {
-        this.setState({loading: true})
+    fetchStories = () => {
+        this.setState({ loading: true })
         fetch(`${apiRoot}/stories?author=${this.state.user.id}`, {
             mode: "cors",
             headers: new Headers({
                 "Authorization": localStorage.getItem(storageKey)
             })
         })
-             .then(resp => {
+            .then(resp => {
                 if (resp.ok) {
                     return resp.json()
                 } else {
@@ -39,19 +37,70 @@ class AccountContainer extends Component {
                 }
             })
             .then(data => {
-                this.setState({stories: data, loading: false})
+                this.setState({ stories: data, loading: false })
                 return data
             })
             .catch(err => {
-                this.setState({loading: false})
+                this.setState({ loading: false })
                 console.log(err)
                 return null
             })
     }
 
+
+    showmodal = (dimmer) => () => this.setState({ dimmer, openModal: true })
+    closemodal = () => this.setState({ openModal: false })
+
+    // Event handler that triggers when delete butotn is clicked on delete prompt window
+    // deletes the section 
+    handleDeleteStory = () => {
+        fetch(`${apiRoot}/stories/${this.state.selectedstoryid}`, {
+            mode: "cors",
+            method: "DELETE",
+            headers: new Headers({
+                "Authorization": localStorage.getItem(storageKey)
+            })
+        })
+            .then(resp => {
+                if (resp.ok) {
+                    return resp.json()
+                } else {
+                    return Promise.reject({
+                        status: resp.status,
+                        statusText: resp.statusText,
+                        statusMessage: resp.text()
+                    })
+                }
+            })
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.log(err)
+                return null
+            })
+        this.setState({ deleteModalOpen: false })
+        this.fetchStories()
+    }
+
+    // Event handler that open/close the delete prompt modal
+    showDeleteModal = () => () => this.setState({ deleteModalOpen: true })
+    closeDeleteModal = () => this.setState({ deleteModalOpen: false })
+
+    componentDidMount = () => {
+        this.fetchStories()
+    }
+
     render() {
         console.log("Rendering Account comp. State: ", this.state)
-        return <Account {...this.state} showmodal={this.showmodal} closemodal={this.closemodal} />
+        return <Account
+            {...this.state}
+            showmodal={this.showmodal}
+            closemodal={this.closemodal}
+            showDeleteModal={this.showDeleteModal}
+            closeDeleteModal={this.closeDeleteModal}
+            handleDeleteStory={this.handleDeleteStory}
+        />
     }
 }
 
