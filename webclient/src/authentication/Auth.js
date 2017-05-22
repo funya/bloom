47f5536a53e29a4143bef3a51fb254713ccbe47a
John Diego <jadiego@uw.edu>
2017-05-22 16:59:04 -0700

    change apiroot

--- webclient/src/authentication/Auth.js
+++ webclient/src/authentication/Auth.js
@@ -2,7 +2,10 @@ import 'whatwg-fetch'
 
 export const storageKey = 'auth'
 
-export const apiRoot = "https://localhost:4000/v1"
+export var apiRoot = "https://api.chayabloom.me/v1/";
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+  apiRoot = "https://localhost:4000/v1/"
+};
 
 export const Auth = {
     authenticate(f, u, p) {
