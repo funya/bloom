b25f54d2bfc113c523b40ea27349121894455711
John Diego <jadiego@uw.edu>
2017-05-24 01:05:01 -0700

    reduxify edit page

--- webclient/src/edit/EditContainer.js
+++ webclient/src/edit/EditContainer.js
@@ -1,57 +1,29 @@
 import React, { Component } from 'react';
-import 'whatwg-fetch'
+import { Container, Segment, Icon, Input, Button, Loader, Dimmer, Modal, Card } from 'semantic-ui-react';
 import { arrayMove } from 'react-sortable-hoc';
-import { apiRoot, storageKey } from '../authentication/Auth'
 import './edit.css';
-import Edit from './Edit'
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import AddSectionButton from './AddSectionButton';
+import EditableSectionItem from './EditableSectionItem';
+import { Link } from 'react-router-dom';
+import { isEqual } from 'lodash';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { getSections, getMyStories, setCurrentStory } from '../redux/actions';
+
 
 class EditContainer extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            story: this.props.location.state,
-            timelineitems: [],
-        };
-    }
-
-    //Fetch Sections
-    fetchSections = () => {
-        this.setState({ loadingSections: true })
-        // Get the single story information 
-        fetch(`${apiRoot}/stories/${this.state.story.id}`, {
-            mode: "cors",
-            headers: new Headers({
-                "Authorization": localStorage.getItem(storageKey)
-            })
-        })
-            .then(resp => {
-                if (resp.ok) {
-                    return resp.json()
-                } else {
-                    return Promise.reject({
-                        status: resp.status,
-                        statusText: resp.statusText,
-                        statusMessage: resp.text()
-                    })
-                }
-            })
-            .then(data => {
-                this.setState({ timelineitems: data, loadingSections: false })
-            })
-            .catch(err => {
-                this.setState({ loadingSections: false })
-                console.log(err)
-                return null
-            })
-    }
 
     // Initialization of DOM nodes goes here. When the edit page is mounted, a fetch 
     // call is made to get the sections of the story. 
     componentDidMount() {
-        this.fetchSections()
-    }
+        let storyid = this.props.match.params.id
 
+        this.props.setCurrentStory(storyid)
+        this.props.getSections(storyid)
+
+    }
 
     // Event handler that manages the changing of the story title
     handleTimelineTitleChange = (event) => this.setState({ title: event.target.value })
@@ -59,22 +31,102 @@ class EditContainer extends Component {
 
     // Callback called when resorting of section ends
     onSortEnd = ({ oldIndex, newIndex }) => {
-        // console.log("Oldindex",oldIndex, this.state.timelineitems)
-        // console.log("Newindex",newIndex, this.state.timelineitems)
-        this.setState({
-            timelineitems: arrayMove(this.state.timelineitems, oldIndex, newIndex),
-        });
+        let items = this.props.sections[this.props.currentStory.id]
+        console.log(items)
+        let itemsnew = arrayMove(items, oldIndex, newIndex)
+        itemsnew.map((ele, index) => {
+            ele.index = index
+        })
+        console.log(itemsnew)
+        //MAKE THIS items move store in the DB by making fetch call
     };
 
     render() {
-        console.log("Rendering Edit comp. State: ", this.state)
-        return (
-            <Edit
-                {...this.state}
-                fetchSections={this.fetchSections}
-            />
-        )
-    }
-}
+        const { fetching, fetchError, currentStory, sections } = this.props
 
-export default EditContainer
+        let items = sections[currentStory.id]
+
+
+        // Renders a Next button depending the length of sections within the story
+        // a single story has exist before it could be rendered.
+        let nextbutton = null
+        items !== undefined && (
+            items.length > 0 && (
+                nextbutton = <Link className='ui button preview-story-button' to={`/story/${currentStory.id}/preview`}>
+                    Next
+                <Icon name='long arrow right' />
+                </Link>
+            )
+        )
+            return (
+                <Container fluid id="editcontainer">
+                    <Container>
+                        <Segment basic textAlign='right'>
+                            <Button>See Demo</Button>
+                            {nextbutton}
+                        </Segment>
+                        <Segment padded='very' basic>
+                            <Input id='timeline-title' fluid
+                                placeholder='Title'
+                                onChange={this.handleTimelineTitleChange}
+                            />
+                        </Segment>
+                        <Segment padded='very' basic className='section-grid-container'>
+                            <Dimmer active={fetching.count !== 0} inverted>
+                                <Loader inverted content='Loading Sections' />
+                            </Dimmer>
+                            {items !== undefined && (
+                                <SortableList
+                                    items={items}
+                                    onSortEnd={this.onSortEnd}
+                                    axis='xy'
+                                    helperClass='section-grid-item-wrapper'
+                                    pressDelay={200}
+                                />
+                            )
+                            }
+                        </Segment>
+                    </Container>
+                </Container>
+            )
+        }
+    }
+
+    const SortableItem = SortableElement(({ value }) => {
+        return (
+            <EditableSectionItem value={value} />
+        )
+    });
+
+    const SortableList = SortableContainer(({ items }) => {
+        return (
+            <div className='section-grid'>
+                {items.map((value, index) => (
+                    <SortableItem
+                        key={`item-${index}`}
+                        index={index} value={value}
+                    />
+                ))}
+                <AddSectionButton />
+            </div>
+        );
+    });
+
+    const mapStateToProps = (state) => {
+        return {
+            fetching: state.fetching,
+            fetchError: state.fetchError,
+            sections: state.sections,
+            currentStory: state.currentStory,
+        }
+    }
+
+    const mapDispatchToProps = (dispatch) => {
+        return bindActionCreators({
+            getSections,
+            getMyStories,
+            setCurrentStory
+        }, dispatch)
+    }
+
+    export default connect(mapStateToProps, mapDispatchToProps)(EditContainer)
