cec74b78a399799190c080c3668a3b8e3a6ade05
John Diego <jadiego@uw.edu>
2017-05-24 01:03:20 -0700

    add redux to index

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -7,7 +7,21 @@ import './index.css';
 //app
 import App from './components/App';
 
+//redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers';
+
+//store setup for redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 ReactDOM.render(
-    <App/>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
