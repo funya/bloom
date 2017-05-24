b12526e33b40388fd67010b16accf4ce6d4ff5f6
John Diego <jadiego@uw.edu>
2017-05-24 01:04:14 -0700

    reduxify auth

--- webclient/src/components/App.js
+++ webclient/src/components/App.js
@@ -1,73 +1,40 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react';
+import { isEmpty } from 'lodash';
 
 //components
-import Navbar from './Navbar'
-import Footer from './Footer'
-import { isAuthenticated, apiRoot, storageKey} from '../authentication/Auth'
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 //routing components
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 //pages
-import Home from '../home/Home'
-import About from '../about/About'
-import Account from '../account/AccountContainer'
-import Story from './Story'
-import Edit from '../edit/EditContainer'
-import Signup from '../authentication/Signup'
-import Login from '../authentication/Login'
-import Preview from '../preview/PreviewContainer'
-import Confirmation from '../preview/Confirmation'
+import Home from '../home/Home';
+import About from '../about/About';
+import Account from '../account/AccountContainer';
+import Story from './Story';
+import Edit from '../edit/EditContainer';
+import Signup from '../authentication/Signup';
+import Login from '../authentication/Login';
+import Preview from '../preview/PreviewContainer';
+import Confirmation from '../preview/Confirmation';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { checkSession } from '../redux/actions';
+
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            user: {},
-            isAuthenticated: false
-        }
-    }
-
     componentDidMount() {
-        fetch(`${apiRoot}/users/me`, {
-            mode: "cors",
-            headers: new Headers({
-                "Authorization": localStorage.getItem(storageKey)
-            })
-        })
-            .then(resp => {
-                if (resp.ok) {
-                    return resp.json()
-                } else {
-                    return Promise.reject({
-                        status: resp.status,
-                        statusText: resp.statusText,
-                        statusMessage: resp.text()
-                    })
-                }
-            })
-            .then(data => {
-                this.setState({user: data, isAuthenticated: true})
-                localStorage.setItem("u", JSON.stringify(data))
-                return data
-            })
-            .catch(err => {
-                console.log(err)
-                localStorage.removeItem("u")
-                localStorage.removeItem("auth")
-                this.forceUpdate()
-                return null
-            })
+        this.props.checkSession()
     }
 
 
     render() {
-        console.log("Rendering App comp. State: ", this.state)
         const AuthRoute = ({ component: Component, ...rest }) => (
             <Route {...rest} render={props => (
-                localStorage.getItem("u") && localStorage.getItem("auth") ? (
+                !isEmpty(this.props.currentUser) && localStorage.getItem("auth") ? (
                     <Component {...props} />
                 ) : (
                         <Redirect to={{
@@ -81,7 +48,7 @@ class App extends Component {
         return (
             <BrowserRouter forceRefresh={!('pushState' in window.history)}>
                 <Container fluid id='app'>
-                    <Navbar {...this.state}/>
+                    <Navbar />
                     <div>
                         <Route exact path="/" component={Home} />
                         <Route exact path="/about" component={About} />
@@ -102,4 +69,16 @@ class App extends Component {
     }
 }
 
-export default App
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.currentUser,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        checkSession,
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
